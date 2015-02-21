6c4a21d6565e88142d4d264552a1c86951d44bca
btolsch <btolsch@gmail.com>
2015-02-20 23:17:17 -0500

    Another error msg fix.

--- slim_install.sh
+++ slim_install.sh
@@ -4,8 +4,7 @@ NEWBG=~/pictures7/Wallpapers/Misc/linux-www-wallpaper-hd-computer-photo-linux-wa
 if [ -f $BG ]; then
 	if [ ! -L $BG ]; then
 		mv $BG ${BG}.bak 2>/dev/null ||
-		  (echo "You don't have permission to write to ${BG%background.jpg}" &&
-		  exit 1)
+		  echo "You don't have permission to write to ${BG%background.jpg}" && exit 1
 	fi
 	ln -sf $NEWBG $BG 2>/dev/null || 
 	  echo "You don't have permission to write to ${BG%background.jpg}"
