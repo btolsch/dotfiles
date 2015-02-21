449704738a15dad2149ec6791de1e3bdd7c6430f
btolsch <btolsch@gmail.com>
2015-02-20 23:27:40 -0500

    Another attempt at slim install error handling.

--- slim_install.sh
+++ slim_install.sh
@@ -3,8 +3,8 @@ BG=/usr/share/slim/themes/default/background.jpg
 NEWBG=~/pictures7/Wallpapers/Misc/linux-www-wallpaper-hd-computer-photo-linux-wallpaper.jpg
 if [ -f $BG ]; then
 	if [ ! -L $BG ]; then
-		mv $BG ${BG}.bak 2>/dev/null ||
-		  echo "You don't have permission to write to ${BG%background.jpg}" && exit 1
+		mv -f $BG ${BG}.bak 2>/dev/null ||
+		  { echo "You don't have permission to write to ${BG%background.jpg}"; exit 1}
 	fi
 fi
 ln -sf $NEWBG $BG 2>/dev/null || 
