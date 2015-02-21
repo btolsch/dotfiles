5962d850a9674c27f94c5845368ae14d6d47a032
btolsch <btolsch@gmail.com>
2015-02-20 23:13:19 -0500

    Silenced errors in slim bg install scripts.

--- slim_install.sh
+++ slim_install.sh
@@ -3,10 +3,10 @@ BG=/usr/share/slim/themes/default/background.jpg
 NEWBG=~/pictures7/Wallpapers/Misc/linux-www-wallpaper-hd-computer-photo-linux-wallpaper.jpg
 if [ -f $BG ]; then
 	if [ ! -L $BG ]; then
-		mv $BG ${BG}.bak ||
+		mv $BG ${BG}.bak 2>/dev/null ||
 		  (echo "You don't have permission to write to ${BG%background.jpg}" &&
 		  exit 1)
 	fi
-	ln -sf $NEWBG $BG || 
+	ln -sf $NEWBG $BG 2>/dev/null || 
 	  echo "You don't have permission to write to ${BG%background.jpg}"
 fi
