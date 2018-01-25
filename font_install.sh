576fb7e78234c0258cdc15b75f8180a64f8cbf9f
btolsch <btolsch@google.com>
2018-01-25 13:49:40 -0800

    Add check for google-specific bitmap font exclude

--- font_install.sh
+++ font_install.sh
@@ -1,2 +1,5 @@
 #!/bin/zsh
 fc-cache -vf ~/.fonts >/dev/null
+if [ -e /etc/fonts/conf.d/70-no-bitmaps.conf ]; then
+  echo "Warning: remove /etc/fonts/conf.d/70-no-bitmaps.conf to use bitmap fonts like Terminess Powerline"
+fi
