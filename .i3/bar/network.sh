7c64f1269eb3f8006735416fd875614f80e75fe8
Brandon Tolsch <btolsch@gmail.com>
2019-04-14 20:20:53 -0700

    Fix dropbox start + net list

--- .i3/bar/network.sh
+++ .i3/bar/network.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ip addr | awk '
   /^[0-9]+:.*state UP/ { x=substr($2,0,length($2)-1) }
+  /^[0-9]+:.*POINTOPOINT/ { x=substr($2,0,length($2)-1) }
   /inet .*global/ {
     sub(/\/[0-9]+/, "", $2)
     print x, $2
--- bin/autostart.sh
+++ bin/autostart.sh
@@ -3,6 +3,7 @@
 source ~/.fehbg
 xset r rate 150 25
 xinput set-prop 14 268 2
+(ps uxc | grep dropbox) || dropbox
 #and look into options like ncache and the repainting options
 #also maybe want to save a couple past logs...
 #x11vnc -display $DISPLAY -usepw -o ~/.vnc/x11vnc.log -forever &>~/.vnc/x11vnc.port &
