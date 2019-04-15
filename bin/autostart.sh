06e40ca5a903ab7779bb344b2752c1a53cdf0fca
Brandon Tolsch <btolsch@gmail.com>
2019-04-14 20:20:53 -0700

    Fix dropbox start + net list

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -6,6 +6,7 @@ xinput set-prop 14 268 2
 systemctl --user start redshift-gtk
 # Don't run xscreensaver on chrome remote desktop
 [[ "$DISPLAY" != ":0" ]] && xscreensaver-command -exit
+(ps uxc | grep dropbox) || dropbox
 #and look into options like ncache and the repainting options
 #also maybe want to save a couple past logs...
 #x11vnc -display $DISPLAY -usepw -o ~/.vnc/x11vnc.log -forever &>~/.vnc/x11vnc.port &
