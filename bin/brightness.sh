5e9cea019da7457280903138fdbb370369ccb8dd
Brandon Tolsch <btolsch@gmail.com>
2019-05-31 19:04:06 -0700

    Increase brightness notification timeout

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 xbacklight $@
-notify-send --urgency=low -t 800 "brightness $(xbacklight -get | sed 's/\..*$//')"
+notify-send --urgency=low -t 1500 "brightness $(xbacklight -get | sed 's/\..*$//')"
