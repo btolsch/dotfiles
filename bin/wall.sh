c47478bfe950e134f542beb1b3dd8148a2461ebe
Brandon Tolsch <btolsch@gmail.com>
2019-06-04 23:22:03 -0700

    Update wallpaper only on existing displays

--- bin/wall.sh
+++ bin/wall.sh
@@ -2,11 +2,11 @@
 PID=$(pgrep gnome-session)
 file=$(~/bin/wall.py)
 if [[ $file ]]; then
-	if [ $PID ]; then
-		export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-		gsettings set org.gnome.desktop.background picture-uri file://$file
-	fi
-	DISPLAY=:0 feh --bg-scale $file
-	DISPLAY=:20 feh --bg-scale $file
-	#ln -sf $file /usr/share/slim/themes/default/background.jpg
+  if [ $PID ]; then
+    export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+    gsettings set org.gnome.desktop.background picture-uri file://$file
+  fi
+  for disp in $(cd /tmp/.X11-unix && for x in X*; do echo ":${x#X}"; done); do
+    DISPLAY=$disp feh --bg-scale $file
+  done
 fi
