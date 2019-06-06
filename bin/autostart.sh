e6a3b6c39000cac5bfc6f6c4169eb9c98e27e062
Brandon Tolsch <btolsch@gmail.com>
2019-06-05 22:02:19 -0700

    Fix some autostart.sh funkiness

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 source ~/.fehbg
-xinput set-prop 14 268 2
 systemctl --user start redshift-gtk
+pidof -x xscreensaver &> /dev/null || xscreensaver -nosplash &
 # Don't run xscreensaver on chrome remote desktop
 [[ "$DISPLAY" != ":0" ]] && xscreensaver-command -exit
 pidof -x dropbox &>/dev/null || dropbox
-pidof -x xscreensaver &> /dev/null || xscreensaver -nosplash &
 pidof -x conky &> /dev/null || conky &
 pidof -x udiskie &> /dev/null || udiskie -2 --tray &
 #and look into options like ncache and the repainting options
