3a62c7b00a5fda0b0a0fe15e89700178799427b3
Brandon Tolsch <btolsch@gmail.com>
2019-06-06 17:18:32 -0700

    Make a stricter attempt at killing lemonbar procs

--- bin/kill-lemonbar.sh
+++ bin/kill-lemonbar.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
-pidof -x bar.zsh &> /dev/null && kill -9 -- -$(cat /proc/$(pgrep bar.zsh | head -1)/stat | awk '{ print $5 }')
+while pidof -x bar.zsh &> /dev/null; do
+  PIDS=$(for PPID_ in $(pgrep bar.zsh); do
+    pgrep -P $PPID_
+  done)
+  for PPID_ in $(pgrep bar.zsh); do
+    kill -9 $PPID_
+  done
+  for PID_ in $(echo $PIDS); do
+    kill -9 $PID_;
+  done
+done
