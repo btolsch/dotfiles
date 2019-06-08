e54fb4697bb4d660a6125e9702a02b1d200f668c
btolsch <btolsch@google.com>
2019-06-07 18:13:14 -0700

    Add a path that reverses the file dict direction

--- install.sh
+++ install.sh
@@ -15,40 +15,72 @@ for script in $(ls | grep -E ".install\.sh"); do
   source $script $2
 done
 
-batch_dict_symlink_install() {
-  local -A files
-  files=($@)
-  for file in ${(k)files}; do
-    if [ "${platform_symlink_files[$file]}" = "" ]; then
-      dest_file=~/${files[$file]}
-      symlink_install $dest_file $file $OVERRIDE
-    fi
-  done
-}
-
 batch_symlink_install() {
   batch_dict_symlink_install $(paste -d ' ' <(echo $@ | tr ' ' '\n') <(echo $@ | tr ' ' '\n') | tr '\n' ' ')
 }
 
-# TODO(btolsch): This should really be dest file first so e.g. .vim and .vimrc
-# can be linked to two places.
-typeset -A other_files
-other_files=(
-    ".vim" ".config/nvim"
-    ".vimrc" ".config/nvim/init.vim"
-    "mpd.conf" ".config/mpd/mpd.conf"
-    "powerline" ".config/powerline"
-    "redshift-gtk.service" ".config/systemd/user/redshift-gtk.service"
-    "redshift-gtk.service.d" ".config/systemd/user/redshift-gtk.service.d"
-    "redshift.conf" ".config/redshift/redshift.conf"
-    "sxhkd/normal" ".config/sxhkd/sxhkdrc"
-)
-batch_symlink_install $(ls -d .* | grep -v ".git")
-batch_symlink_install bin/*
-batch_dict_symlink_install ${(kv)other_files}
-for file in ${(k)platform_symlink_files}; do
-  dest_file=~/${platform_symlink_files[$file]}
-  symlink_install $dest_file $PLATFORM_REL_PREFIX/${platform_symlink_prefixes[$file]}/$file $1
-done
+# TODO(btolsch): Rip this out after everyone is ported to the reversed dict
+# sense.
+if [[ -n $PORTED_FILE_DICT ]]; then
+  batch_dict_symlink_install() {
+    local -A files
+    files=($@)
+    for dest_file in ${(k)files}; do
+      if [ "${platform_symlink_files[$dest_file]}" = "" ]; then
+        file=${files[$dest_file]}
+        symlink_install ~/$dest_file $file $OVERRIDE
+      fi
+    done
+  }
+
+  typeset -A other_files
+  other_files=(
+      ".config/nvim" ".vim"
+      ".config/nvim/init.vim" ".vimrc"
+      ".config/mpd/mpd.conf" "mpd.conf"
+      ".config/powerline" "powerline"
+      ".config/systemd/user/redshift-gtk.service" "redshift-gtk.service"
+      ".config/systemd/user/redshift-gtk.service.d" "redshift-gtk.service.d"
+      ".config/redshift/redshift.conf" "redshift.conf"
+      ".config/sxhkd/sxhkdrc" "sxhkd/normal"
+  )
+  batch_symlink_install $(ls -d .* | grep -v ".git")
+  batch_symlink_install bin/*
+  batch_dict_symlink_install ${(kv)other_files}
+  for dest_file in ${(k)platform_symlink_files}; do
+    file=${platform_symlink_files[$dest_file]}
+    symlink_install ~/$dest_file $PLATFORM_REL_PREFIX/${platform_symlink_prefixes[$dest_file]}/$file $1
+  done
+else
+  batch_dict_symlink_install() {
+    local -A files
+    files=($@)
+    for file in ${(k)files}; do
+      if [ "${platform_symlink_files[$file]}" = "" ]; then
+        dest_file=~/${files[$file]}
+        symlink_install $dest_file $file $OVERRIDE
+      fi
+    done
+  }
+
+  typeset -A other_files
+  other_files=(
+      ".vim" ".config/nvim"
+      ".vimrc" ".config/nvim/init.vim"
+      "mpd.conf" ".config/mpd/mpd.conf"
+      "powerline" ".config/powerline"
+      "redshift-gtk.service" ".config/systemd/user/redshift-gtk.service"
+      "redshift-gtk.service.d" ".config/systemd/user/redshift-gtk.service.d"
+      "redshift.conf" ".config/redshift/redshift.conf"
+      "sxhkd/normal" ".config/sxhkd/sxhkdrc"
+  )
+  batch_symlink_install $(ls -d .* | grep -v ".git")
+  batch_symlink_install bin/*
+  batch_dict_symlink_install ${(kv)other_files}
+  for file in ${(k)platform_symlink_files}; do
+    dest_file=~/${platform_symlink_files[$file]}
+    symlink_install $dest_file $PLATFORM_REL_PREFIX/${platform_symlink_prefixes[$file]}/$file $1
+  done
+fi
 
 popd
--- platform_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-[ -e platform/install.sh ] && source platform/install.sh
